8f97dc441296d0aa4315fbcabafd4492855ca9da
Ionut Gherman <ghermanionut96@gmail.com>
2024-09-09 15:39:36 +0300

    zuma: build DeviceAsWebcam
    Signed-off-by: Ionut Gherman <ghermanionut96@gmail.com>

--- common.mk
+++ common.mk
@@ -431,6 +431,13 @@ TARGET_EXCLUDES_AUDIOFX := true
 # BCR Call Recording
 $(call inherit-product-if-exists, vendor/bcr/bcr.mk)
 
+# DeviceAsWebcam
+TARGET_BUILD_DEVICE_AS_WEBCAM := true
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/lib64/libjni_deviceAsWebcam.so \
+    system/priv-app/DeviceAsWebcam/DeviceAsWebcam.apk \
+    system/priv-app/DeviceAsWebcam/lib/arm64/libjni_deviceAsWebcam.so
+
 # Google Camera
 $(call inherit-product-if-exists, vendor/google/camera/camera.mk)
 
