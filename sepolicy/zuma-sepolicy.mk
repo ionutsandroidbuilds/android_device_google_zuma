a2189ab23340f6ae03c4cbdf08b75187bc970fa0
Ionut Gherman <ghermanionut96@gmail.com>
2025-01-02 10:29:34 +0200

    zuma: add missing wifi_ext sepolicy
    Signed-off-by: Ionut Gherman <ghermanionut96@gmail.com>

--- sepolicy/zuma-sepolicy.mk
+++ sepolicy/zuma-sepolicy.mk
@@ -4,6 +4,9 @@ BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/connectivity_thermal_power
 # google_battery service
 BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/googlebattery
 
+# wifi_ext service
+BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/wifi_ext
+
 # sepolicy that are shared among devices using zuma
 BOARD_SEPOLICY_DIRS += device/google/zuma/sepolicy/vendor
 BOARD_VENDOR_SEPOLICY_DIRS += device/google/zuma/sepolicy/radio
