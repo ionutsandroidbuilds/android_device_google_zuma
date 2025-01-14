7aec5fae976b914a8e1a2e502c6187c08994c07c
Ionut Gherman <ghermanionut96@gmail.com>
2025-01-14 22:48:10 +0200

    zuma: add missing turbo_adapter sepolicy
    Signed-off-by: Ionut Gherman <ghermanionut96@gmail.com>

--- sepolicy/zuma-sepolicy.mk
+++ sepolicy/zuma-sepolicy.mk
@@ -4,6 +4,9 @@ BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/connectivity_thermal_power
 # google_battery service
 BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/googlebattery
 
+# turbo_adapter service
+BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/turbo_adapter
+
 # wifi_ext service
 BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/wifi_ext
 
