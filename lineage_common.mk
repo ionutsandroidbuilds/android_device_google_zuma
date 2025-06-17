e17d9e5f3927290ea800975ef7035340436e7fb1
Ionut <ghermanionut96@gmail.com>
2025-06-17 19:37:35 +0300

    Revert "zuma: Migrate Lineage Health to soong_config_set"
    This reverts commit 0761e205fc5f42d8512efc52c09ce81ee9bd0984.

--- lineage_common.mk
+++ lineage_common.mk
@@ -35,10 +35,6 @@ $(call inherit-product-if-exists, vendor/google/faceunlock/config.mk)
 # Lineage Health
 include hardware/google/pixel/lineage_health/device.mk
 
-$(call soong_config_set,lineage_health,charging_control_supports_deadline,true)
-$(call soong_config_set,lineage_health,charging_control_supports_limit,true)
-$(call soong_config_set,lineage_health,charging_control_supports_toggle,false)
-
 # Linker config
 PRODUCT_VENDOR_LINKER_CONFIG_FRAGMENTS += \
     device/google/zuma/linker.config.json
