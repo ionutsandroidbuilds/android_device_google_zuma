306e087c29d3e837994b35ae4231c98d8cf81029
Ionut <ghermanionut96@gmail.com>
2026-01-08 00:14:19 +0200

    zuma: disable artifact path requirements
    Signed-off-by: Ionut <ghermanionut96@gmail.com>

--- common.mk
+++ common.mk
@@ -433,6 +433,9 @@ TARGET_EXCLUDES_AUDIOFX := true
 # BCR Call Recording
 $(call inherit-product-if-exists, vendor/bcr/bcr.mk)
 
+# Disable artifact path requirements
+DISABLE_ARTIFACT_PATH_REQUIREMENTS := true
+
 # Extras for Pixels
 $(call inherit-product-if-exists, vendor/google/pixels_extras/pixels_extras.mk)
 
