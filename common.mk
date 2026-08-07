3a39d12a09149ae8101944a602a906e18ba82168
Ionut <ghermanionut96@gmail.com>
2026-08-08 00:19:01 +0300

    zuma: move GlanceableHub overlays to addons
    * already preset in vendor/addons
    Signed-off-by: Ionut <ghermanionut96@gmail.com>

--- common.mk
+++ common.mk
@@ -326,10 +326,6 @@ PRODUCT_PACKAGES += \
     EuiccSupportPixelOverlay \
     FrameworkResOverlayProductZuma \
     FrameworkResOverlayVendorZuma \
-    GlanceableHubConfigOverlay \
-    GlanceableHubSettingsConfigOverlay \
-    GlanceableHubSettingsConfigOverlay2022 \
-    GlanceableHubSysuiConfigOverlay \
     GoogleConfigOverlay \
     GooglePermissionControllerSafetyCenterOverlay \
     PixelConfigOverlay2019 \
